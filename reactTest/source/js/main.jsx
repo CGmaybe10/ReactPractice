65e8040accdabea4ce9d05834e6e7815200618ff
moubiao <4099587876@qq.com>
2018-04-25 21:49:53 +0800

    添加事件处理

--- reactTest/source/js/main.jsx
+++ reactTest/source/js/main.jsx
@@ -2,8 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Qiang extends React.Component {
+    handleClick() {
+        console.log("moubiao", 'onClick---->');
+    }
+
     render() {
-        return <h1>qiang zhong qiang!</h1>;
+        return (
+            <div>
+                <h1>qiang zhong qiang!</h1>
+                <input type='button' value='button' onClick={this.handleClick} />
+                <Toggle />
+            </div>
+        )
+    }
+}
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
+        this.toggleClick = this.toggleClick.bind(this);
+    }
+
+    toggleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.toggleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
     }
 }
 
