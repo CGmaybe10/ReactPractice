1704444cbc4afc4064484349e527655bb1469bce
牟彪 <biao.mou@inin88.com>
2018-04-25 17:02:56 +0800

    添加第一个demo

---
+++ reactTest/source/js/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Qiang extends React.Component {
+    render() {
+        return <h1>qiang zhong qiang!</h1>;
+    }
+}
+
+ReactDOM.render(<Qiang />, document.getElementById('root'));
