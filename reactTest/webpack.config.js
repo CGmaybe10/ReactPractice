62f228b2d38725c2a428d54f5964d49c7e20cc34
牟彪 <biao.mou@inin88.com>
2018-05-04 17:40:26 +0800

    添加less-loader的依赖

--- reactTest/webpack.config.js
+++ reactTest/webpack.config.js
@@ -15,11 +15,22 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /(\.jsx|\.js)$/,
-            use: {
-                loader: "babel-loader",
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                },
+                exclude: /node_modules/
             },
-            exclude: /node_modules/
-        }]
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            }
+        ]
     }
 }
