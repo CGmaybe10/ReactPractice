3f753a3ac1f43d9d30d9ac910ba104ad7b66cecb
牟彪 <biao.mou@inin88.com>
2018-05-24 17:44:30 +0800

    创建新的工程

---
+++ fundsDemo/webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    entry: {
+        main: __dirname + "/source/js/main.jsx",
+    },
+    output: {
+        path: __dirname + "/build",
+        filename: "index.js"
+    },
+    devtool: "source-map",
+    mode: "development",
+    devServer: {
+        contentBase: "./build",
+        historyApiFallback: true,
+        inline: true
+    },
+    module: {
+        rules: [{
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /(\.less|\.css)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader", // translates CSS into CommonJS
+                    options: {
+                        modules: "true",
+                        localIdentName: '[name]__[local]--[hash:base64:5]'
+                    }
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            }
+        ]
+    },
+
+}
