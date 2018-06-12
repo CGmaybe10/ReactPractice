a6e603d34e1b157dba2821f35f6b4bc1daa71c82
牟彪 <biao.mou@inin88.com>
2018-06-12 10:44:06 +0800

    添加子控件向父控件传递数据的例子

--- reactTest/source/js/FatherComponent.jsx
+++ reactTest/source/js/FatherComponent.jsx
@@ -4,13 +4,25 @@ import { GrandsonComponent } from './GrandsonComponent.jsx';
 class FatherComponent extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            childData: '无数据',
+        }
+    }
+
+    handleChildMsg(msg) {
+        console.log('收到了来自子控件的数据：' + msg);
+        this.setState({
+            childData: msg,
+        });
     }
 
     render() {
         return (
             <div>
                 <h4>这是父控件-传递的数据是：I am your father</h4>
-                <GrandsonComponent {...this.props} fatherData="I am your father" />
+                <p>等待子控件的数据：{this.state.childData}</p>
+                <GrandsonComponent {...this.props} fatherData="I am your father" transferMsg={msg => this.handleChildMsg(msg)} />
             </div>
         );
     }
--- reactTest/source/js/GrandsonComponent.jsx
+++ reactTest/source/js/GrandsonComponent.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 class GrandsonComponent extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        let sonData = 'I am a child';
+        console.log('子控件传递的数据是：' + sonData);
+
+        this.props.transferMsg(sonData);
     }
 
     render() {
@@ -12,6 +21,7 @@ class GrandsonComponent extends React.Component {
                 <p>来自父控件的数据: {this.props.fatherData}</p>
                 <p>来自祖父控件的数据：{this.props.grandfatherData}</p>
                 <p>来自root控件的数据：{this.props.rootData}</p>
+                <button onClick={this.handleClick}>按这个按钮向父控件传递数据</button>
             </div>
         );
     }
